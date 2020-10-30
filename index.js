fe631379b6786dc4dd6d98f41a7b527678b607a5
Isadora Almeida <isadora.rocha.de.almeida@gmail.com>
2020-10-30 00:52:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`INPUT[data-key="${e.keyCode}"]`)
+
+    if (!audio) {
+        return
+    }
+
+    key.classList.add('clicked')
+    audio.currentTime = 0
+    audio.play()
+}
+
+function playSoundClick(e) {
+    const audio = document.querySelector(`audio[data-key="${e}"]`)
+    const key = document.querySelector(`INPUT[data-key="${e}"]`)
+
+    if (!audio) {
+        return
+    }
+
+    key.classList.add('clicked')
+    audio.currentTime = 0
+    audio.play()
+}
+
+function removeTransition(e) {
+    e.target.classList.remove('clicked')
+}
+
+addEventListener('keydown', playSound)
+const keys = Array.from(document.querySelectorAll('.key'))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
